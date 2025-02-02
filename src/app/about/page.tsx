0116d916419842e525956e97ddf50b61787a0463
siammahamud <siammahamud710@gmail.com>
2025-02-02 23:39:34 +0600

    First commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aboutpage = () => {
+  return (
+    <div className='text-center text-4xl text-green-500 tracking-widest font-bold'>Aboutpage</div>
+  )
+}
+
+export default aboutpage
---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+const Homepage = () => {
+  return (
+    <div  className='text-center text-4xl text-red-500 tracking-widest font-bold'>Homepage</div>
+  )
+}
+
+export default Homepage
